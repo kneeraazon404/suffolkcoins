1e9aa2656ff14e98460a91e6a70a467acc2d44cd
kneeraazon404 <kneeraazon@gmail.com>
2023-10-28 20:55:53 +0545

    first commit

--- inventory/views.py
+++ inventory/views.py
@@ -1,18 +1,9 @@
 import gspread
+import requests
 from django.core.files import File
 from django.shortcuts import render
-from reportlab.lib import colors
-from reportlab.lib.pagesizes import landscape, letter
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.lib.units import inch
-from reportlab.platypus import (
-    Paragraph,
-    SimpleDocTemplate,
-    Spacer,
-    Table,
-    TableStyle,
-    PageBreak,
-)
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 from .models import Inventory
 
@@ -21,79 +12,34 @@ def index(request):
     return render(request, "index.html")
 
 
-custom_page_width = landscape(letter)[0]
-custom_page_height = landscape(letter)[1]
-custom_page_size = (custom_page_width, custom_page_height)
+from django.http import HttpResponse
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
 
 
 def inventory(request):
-    sgs = gspread.service_account("credentials.json")
-    sheet = sgs.open("inventory")
-    worksheet = sheet.get_worksheet(0)
-    raw_data = worksheet.get_all_values()
+    # Step 1: Authenticate using the Service Account
+    SERVICE_ACCOUNT_FILE = "credentials.json"
 
-    # Process the data
-    tables = []
-    current_table = []
-    for row in raw_data:
-        if any(row):
-            if not any(current_table):
-                if len(row) == 1:
-                    tables.append(row[0])
-                else:
-                    current_table.append(row)
-            else:
-                current_table.append(row)
-        else:
-            if any(current_table):
-                tables.append(current_table)
-                current_table = []
+    credentials = Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE,
+        scopes=[
+            "https://www.googleapis.com/auth/spreadsheets.readonly",
+            "https://www.googleapis.com/auth/drive",
+        ],
+    )
 
-    if any(current_table):
-        tables.append(current_table)
+    service = build("drive", "v3", credentials=credentials)
 
-    pdf_path = "static/inventory.pdf"
-    doc = SimpleDocTemplate(pdf_path, pagesize=landscape(custom_page_size))
-    styles = getSampleStyleSheet()
-    title_style = styles["Heading1"]
-    description_style = styles["BodyText"]
-    title_style.alignment = 1
-    description_style.alignment = 1
+    # Step 2: Specify the Google Sheet ID
+    SHEET_ID = "1xLivEMUaUeljApC6JKC-i5GetQGaFr4A-R5Y3y_-y0Q"
 
-    content = []
-    for item in tables:
-        if isinstance(item, str):
-            if len(item.split()) > 5:
-                para = Paragraph(item, description_style)
-            else:
-                para = Paragraph(item, title_style)
-            content.append(para)
-            content.append(Spacer(1, 0.2 * inch))
-        else:
-            colWidths = [len(max(column, key=len)) * 7 for column in zip(*item)]
-            table = Table(item, colWidths=colWidths, repeatRows=1)
-            table_style = [
-                ("BACKGROUND", (0, 0), (-1, 0), colors.black),
-                ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke),
-                ("ALIGN", (0, 0), (-1, -1), "CENTER"),
-                ("FONTNAME", (0, 0), (-1, 0), "Helvetica-Bold"),
-                ("FONTSIZE", (0, 0), (-1, 0), 12),
-                ("BOTTOMPADDING", (0, 0), (-1, 0), 8),
-                ("TOPPADDING", (0, 0), (-1, 0), 8),
-            ]
-            table.setStyle(TableStyle(table_style))
-            content.append(table)
-            content.append(Spacer(1, 0.5 * inch))
+    # Step 3: Export Google Sheet as PDF
+    request = service.files().export_media(fileId=SHEET_ID, mimeType="application/pdf")
+    response = request.execute()
 
-    doc.build(content)
-
-    inventory = Inventory.objects.first()
-    if not inventory:
-        inventory = Inventory()
-
-    with open(pdf_path, "rb") as pdf_file:
-        inventory.file.save("inventory.pdf", File(pdf_file))
-        inventory.save()
-
-    context = {"pdf_path": inventory.file.url}
-    return render(request, "inventory.html", context)
+    # Step 4: Return the PDF as an HTTP response
+    response = HttpResponse(content_type="application/pdf")
+    response["Content-Disposition"] = 'attachment; filename="output.pdf"'
+    response.write(request.execute())
+    return response

e212a1914a5b931bf3302852ed607fa8e553a6e6
kneeraazon404 <kneeraazon@gmail.com>
2023-11-09 23:29:17 +0545

    first commit

--- inventory/views.py
+++ inventory/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from google.oauth2.credentials import Credentials
 
-
-def index(request):
-    return render(request, "index.html")
-
-
 from django.http import HttpResponse
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
+
+from django.http import HttpResponse
+from googleapiclient.discovery import build
+from google.oauth2.service_account import Credentials
+
+
+def index(request):
+    return render(request, "index.html")
 
 
 def inventory(request):
@@ -30,10 +33,10 @@ def inventory(request):
 
     # Step 3: Export Google Sheet as PDF
     request = service.files().export_media(fileId=SHEET_ID, mimeType="application/pdf")
-    response = request.execute()
+    response_content = request.execute()
 
     # Step 4: Return the PDF as an HTTP response
-    response = HttpResponse(content_type="application/pdf")
-    response["Content-Disposition"] = 'attachment; filename="inventory.pdf"'
-    response.write(request.execute())
+    response = HttpResponse(response_content, content_type="application/pdf")
+    # Use 'inline' to open in the browser
+    response["Content-Disposition"] = 'inline; filename="inventory.pdf"'
     return response
